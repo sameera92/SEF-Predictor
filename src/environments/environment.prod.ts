516379d87665c9541d3826350edbcb1ddea9870c
Sameera <sameera.palliyage@blackswan-technologies.com>
2021-08-04 02:28:35 +0530

    update prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
-    production: true,
-    firebase: {
-      apiKey: '...',
-      authDomain: 'project.firebaseapp.com',
-      databaseURL: 'https://project.firebaseio.com',
-      projectId: 'project',
-      storageBucket: 'project.appspot.com',
-      messagingSenderId: '...',
-    },
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCVYOChmB8DYq23FPeUnZDIKQarMxFb748",
+    authDomain: "loginfire-bec83.firebaseapp.com",
+    projectId: "loginfire-bec83",
+    storageBucket: "loginfire-bec83.appspot.com",
+    messagingSenderId: "247645346375",
+    appId: "1:247645346375:web:d4ed8cf2c597a812be6f62",
+    measurementId: "G-Q6PJDY8FHB"
+  }
   };
 
