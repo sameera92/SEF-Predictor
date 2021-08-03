3c1e30a7d1dd092ee04cf131ca74c8103d15aebe
Sameera <sameera.palliyage@blackswan-technologies.com>
2021-08-04 02:04:48 +0530

    theme update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,53 @@ interface Color {
 })
 export class AppComponent {
   title = 'SEF-Predictor';
-  constructor(){
-    }
+  selectedValue: string;
+  bodyContent: any;
+  sourceBody: any;
 
+  constructor(
+    private authenticationService: AuthenticationService
+  ) { }
+
+  ngOnInit() {
+    this.bodyContent = document.getElementById('body-wrapper');
+    this.bodyContent.className = "";
+    this.bodyContent.classList.add('Theme-Black');
+    this.sourceBody = document.getElementsByClassName('source-body');
+    this.sourceBody[0].style.backgroundColor = '#363636';
+  }
+
+  colors: Color[] = [
+    { value: 'BLACK', viewValue: 'Black' },
+    { value: 'BLUE', viewValue: 'Blue' },
+    { value: 'YELLOW', viewValue: 'Yellow' }
+  ];
+
+  onThemeChange(theme: string) {
+    this.bodyContent = document.getElementById('body-wrapper');
+    this.sourceBody = document.getElementsByClassName('source-body');
+
+    switch (theme) {
+
+      case 'BLACK':
+        this.bodyContent.className = "";
+        this.bodyContent.classList.add('Theme-Black');
+        this.sourceBody[0].style.backgroundColor = '#363636';
+        break;
+      case 'BLUE':
+        this.bodyContent.className = "";
+        this.bodyContent.classList.add('Theme-Blue');
+        this.sourceBody[0].style.backgroundColor = '#969be0';
+        break;
+      case 'YELLOW':
+        this.bodyContent.className = "";
+        this.bodyContent.classList.add('Theme-Yellow');
+        this.sourceBody[0].style.backgroundColor = '#ffffaa';
+        break;
+      default:
+        this.bodyContent.className = "";
+        this.bodyContent.classList.add('Theme-Black');
+        break;
+    }
+  }
 }
